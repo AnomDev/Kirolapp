2c57698a8a3a15d55bf5c2c690f4c1263abe5148
Serbla.dev <serbla.dev@gmail.com>
2020-10-17 22:33:07 +0200

    Design works #01
    MaterialDesign added  + Work in Login & Recycler design

--- app/src/main/java/com/serbladev/appfitness/MainActivity.java
+++ app/src/main/java/com/serbladev/appfitness/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-        //Esto es un static final porque es una constante que nunca va a cambiar.
+    //Esto es un static final porque es una constante que nunca va a cambiar.
     public static final String CLAVE_NOMBRE = "NOMBRE";
     EditText etNombre, etPassword;
 
@@ -21,31 +21,31 @@ public class MainActivity extends AppCompatActivity {
 
         //Aquí estamos relacionando la parte de código con la parte visual. Le decimos que nuestra variable etNombre
         //enganche con la vista del layout llamada etNombre
-        etNombre = findViewById(R.id.etNombre);
-        etPassword = findViewById(R.id.etPassword);
+        etNombre = findViewById(R.id.tietName);
+        etPassword = findViewById(R.id.tietPassword);
     }
 
-        // "Linking methods" sólo usarlos con botones. Evitamos así utilizar los onClickListener.
+    // "Linking methods" sólo usarlos con botones. Evitamos así utilizar los onClickListener.
     public void onEntrar(View view) {
-            //Aquí obtenemos lo que se ha escrito en los EditTexts
+        //Aquí obtenemos lo que se ha escrito en los EditTexts
         String nom = etNombre.getText().toString();
         String pas = etPassword.getText().toString();
 
-            //Si coinciden con los requisitos, pasamos a la segunda actividad
+        //Si coinciden con los requisitos, pasamos a la segunda actividad
         if (nom.equals("Pepe") && pas.equals("123")) {
 
-                // Aquí estamos creando el intent que vamos a necesitar para ir a otra actividad
-                // Hay que imaginar un intent como un burro que nos lleva y nos trae, y sus putExtra son las alforjas adicionales que puede llevar
-                // con datos e información entre pantallas.
+            // Aquí estamos creando el intent que vamos a necesitar para ir a otra actividad
+            // Hay que imaginar un intent como un burro que nos lleva y nos trae, y sus putExtra son las alforjas adicionales que puede llevar
+            // con datos e información entre pantallas.
             Intent i = new Intent(MainActivity.this, ListaActivity.class);
-            i.putExtra(CLAVE_NOMBRE , nom);
+            i.putExtra(CLAVE_NOMBRE, nom);
 
-                //Aquí estaría arrancando el intent y pasándonos a la siguiente actividad junto con la información asociada en sus putExtra
+            //Aquí estaría arrancando el intent y pasándonos a la siguiente actividad junto con la información asociada en sus putExtra
             startActivity(i);
 
             //Si no coinciden, muestra un aviso de datos incorrectos.
         } else {
-            Toast.makeText(this,"Los datos no son correctos", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Los datos no son correctos", Toast.LENGTH_LONG).show();
         }
 
     }
--- app/src/main/java/com/serbladev/appfitness/RecyclerAdapter.java
+++ app/src/main/java/com/serbladev/appfitness/RecyclerAdapter.java
@@ -10,17 +10,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MiHolder>{
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MiHolder> {
     ArrayList<Ejercicio> listaEjercicios;
 
     //Aquí le pasamos el arrayList de los elementos de la lista
     public RecyclerAdapter(ArrayList<Ejercicio> listaEjercicios) {
         this.listaEjercicios = listaEjercicios;
+
     }
-    //Aqui cre
-    public static class MiHolder extends  RecyclerView.ViewHolder{
+
+    //Aqui creamos el Holder customizado donde meteremos los elementos que van a ir dentro de cada item de la lista.
+    public static class MiHolder extends RecyclerView.ViewHolder {
         TextView tvFecha, tvActividad, tvDistancia;
-        public MiHolder( View layoutDelItem) {
+
+        public MiHolder(View layoutDelItem) {
             super(layoutDelItem);
             tvFecha = layoutDelItem.findViewById(R.id.tvFecha);
             tvActividad = layoutDelItem.findViewById(R.id.tvActividad);
@@ -28,24 +32,28 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MiHold
         }
     }
 
-    // este metodo coge el layput de la fila y con el construye los elementos dentro del R3ecyclerView
+    // Este método coge el layout de la fila y construye los items dentro del RecyclerView
     @Override
     public MiHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // construyo el layoput de la fila y lo HINCHO para crearlo
-        View filahinchada  =LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_item, parent,false);
+        View filahinchada = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_item, parent, false);
         MiHolder miholder = new MiHolder(filahinchada);
+
+
         return miholder;
     }
 
-    // este metodfose le llama para que pinte cada fila del recycler
-    // recibe el holder mio (donde ya tengo creados los objetos textview) y la posicion de la file que pinto
+    // A este metodo se le llama para que pinte cada fila del RecyclerView,
+    // recibe el holder personal que hemos creado (que ya tiene creados los objetos TextView) además de la posicion de la file que se va a pintar.
     @Override
     public void onBindViewHolder(@NonNull MiHolder holder, int position) {
 
         Ejercicio ej = listaEjercicios.get(position);
         holder.tvFecha.setText(ej.getFecha());
         holder.tvActividad.setText(ej.getTipoActividad());
-        holder.tvDistancia.setText(ej.getDistancia()+"");
+        holder.tvDistancia.setText(ej.getDistancia() + "");
+
+
     }
 
     @Override
